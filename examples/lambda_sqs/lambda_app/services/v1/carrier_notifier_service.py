0ad9c67984af2da8c2fb0e6c95761722d8f9f4c3
Anderson Contreira <anderson.contreira@gmail.com>
2022-01-05 11:06:58 -0300

    Pequenos ajustes em helpers; Ajustes em scripts de migrations;

--- examples/lambda_sqs/lambda_app/services/v1/carrier_notifier_service.py
+++ examples/lambda_sqs/lambda_app/services/v1/carrier_notifier_service.py
@@ -5,6 +5,7 @@ from lambda_app import APP_NAME, APP_VERSION, APP_ARCH_VERSION, helper
 from lambda_app.decorators import SQSEvent
 from lambda_app.decorators.events import SQSRecord
 from lambda_app.events.tracker import EventTracker
+from lambda_app.events_helper import get_records_from_sqs_event, read_event
 from lambda_app.helper import generate_hash
 from lambda_app.logging import get_logger
 from lambda_app.repositories.mysql.ocoren_repository import OcorenRepository
@@ -31,13 +32,13 @@ class CarrierNotifierService:
 
         event_tracker = EventTracker(self.logger)
         event_hash = None
-        # todo extrair essa logica daqui, o servico nao tem que fazer isso
-        records = self.get_records_from_sqs_event(sqs_event)
+
+        records = get_records_from_sqs_event(sqs_event, self.logger)
         if records is not None:
             process_counter = 0
             for record in records:
                 process_counter += 1
-                event = self._read_event(record)
+                event = read_event(record, self.logger)
                 if event is None:
                     raise Exception('Event is None')
                 event_hash = event['hash'] if 'hash' in event else generate_hash(event)
@@ -73,62 +74,3 @@ class CarrierNotifierService:
         event_tracker.track(event_hash, {'result': result})
         self.logger.info('Finishing the process')
         return result
-
-    def _read_event(self, record):
-        self.logger.info('try to reading event form record: {}'.format(record))
-        self.logger.info('Getting type of data: {}'.format(type(record)))
-        try:
-            self.logger.info('dump: {}'.format(json.dumps(record)))
-        except Exception as err:
-            self.logger.error(err)
-        event_body = None
-        try:
-            if isinstance(record, dict):
-                try:
-                    event_body = json.loads(record['body'])
-                except Exception as err:
-                    self.logger.error(err)
-                    unescaped_str = ast.literal_eval(record['body'])
-                    event_body = json.loads(unescaped_str)
-            elif isinstance(record, str):
-                record = json.loads(record)
-                event_body = json.loads(record.body)
-            elif isinstance(record.body, str):
-                event_body = json.loads(record.body)
-            else:
-                event_body = record.body
-        except Exception as err:
-            self.logger.error(err)
-            self.logger.info('event_body: {}'.format(event_body))
-        return event_body
-
-    def get_records_from_sqs_event(self, sqs_event):
-        records = []
-        try:
-            if isinstance(sqs_event, SQSEvent):
-                self.logger.info("SQSEvent instance")
-                if not helper.empty(sqs_event.to_dict()):
-                    try:
-                        sqs_event_dict = sqs_event.to_dict()
-                        if 'Records' in sqs_event_dict:
-                            sqs_event_dict = sqs_event_dict['Records']
-                        for record in sqs_event_dict:
-                            records.append(record)
-                    except Exception as err:
-                        self.logger.error(err)
-                        records.append(sqs_event.to_dict())
-            elif isinstance(sqs_event, SQSRecord):
-                self.logger.info("SQSRecord instance")
-                if not helper.empty(sqs_event.to_dict()):
-                    records.append(sqs_event)
-        except Exception as err:
-            self.logger.error(err)
-            if isinstance(sqs_event, SQSEvent) or isinstance(sqs_event, SQSRecord):
-                self.logger.error(sqs_event.__dict__)
-            else:
-                try:
-                    self.logger.error(json.dumps(sqs_event))
-                except Exception as err:
-                    self.logger.error(err)
-                    self.logger.error(str(sqs_event))
-        return records
